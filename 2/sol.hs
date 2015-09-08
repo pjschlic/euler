45736cc6676fb600adbea4b8787845678116d32e
Peter Schlichting <pjschlic@gmail.com>
2015-09-08 00:16:54 -0700

    format

--- 2/sol.hs
+++ 2/sol.hs
@@ -7,8 +7,8 @@ fib max = fibRest 0 1 max
 -- fibRest :: Num -> Num -> Num -> [Num]
 fibRest last cur max
   | nextCur >= max = []
-	| otherwise = [nextCur] ++ (fibRest cur nextCur max)
-		where nextCur = (last + cur)
+  | otherwise = [nextCur] ++ (fibRest cur nextCur max)
+  where nextCur = (last + cur)
 
 -- main = print $ sum $ filter even $ fib 10
 main = print $ sum $ filter even $ fib (4*10^6)
