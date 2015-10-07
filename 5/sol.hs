7f4ad1a0a1f6678520b6237cd3857728a490f9e6
Pete Schlichting <peteone@google.com>
2015-10-06 17:41:43 -0700

    works!

--- 5/sol.hs
+++ 5/sol.hs
@@ -1,30 +1,35 @@
-import Data.HashTable (HashTable)
+import qualified Data.IntMap as IntMap
 
+-- Find an integer square root of an int.
 isqrt :: Int -> Int
 isqrt = floor . sqrt . fromIntegral
 
-
+-- Factor an int into a list of the factors.
 fact :: Int -> [Int]
 fact x = fact' x 2 (isqrt x)
 
+-- Factor int helper function, will try to find factors up to a max number, then assume it is fully factored.
 fact' :: Int -> Int -> Int -> [Int]
 fact' x cur max
-  | cur > max = [x]
-  | (mod x cur) == 0 = [cur] ++ fact nextCur
-  | otherwise = fact' x (cur+1) max
+  | cur > max = [x] -- cur over max factor suggests that we can stop looking for factors.
+  | (mod x cur) == 0 = [cur] ++ fact nextCur -- cur is a factor, append to list of factors and start factoring quotient.
+  | otherwise = fact' x (cur+1) max -- otherwise try next factor.
   where nextCur = quot x cur
 
-allFactors :: [[Int]] -> [Int]
-allFactors xs = decodeTable $
+-- From an array of ints, find an int which is the smallest common multiple of all elements in array.
+allFactors :: [Int] -> Int
+allFactors xs = foldl (*) 1 $ decodeTable $ joinTables $ map (encodeTable . fact) xs
 
-decodeTable :: HashTable Int Int -> [Int]
-decodeTable table = foldl accumKeyVal [] $ HashTable.toList table
 
-accumKeyVal :: [Int] -> (Int Int) -> [Int]
-accumKeyVal acc (key val) = acc ++ take val $ repeat key
+-- Turn a table which contains factor mapping to count into an array of the factors.
+decodeTable :: IntMap.IntMap Integer -> [Int]
+decodeTable table = foldl (\acc (key, val) -> acc ++ (take (fromIntegral val) $ repeat key)) [] $ IntMap.toList table
 
-encodeTable :: [Int] -> HashTable Int Int
-encodeTable xs = 
+-- Built a map of factor to the number of times it appears.
+-- IE: factors of 8: [2,2,2] => an IntMap of [(2,3)]
+encodeTable :: [Int] -> IntMap.IntMap Integer
+encodeTable xs = IntMap.fromListWith (+) $ map (\x -> (x, 1)) xs
 
-unions :: [Set Int] -> Set Int
-unions xs = foldl (\acc x -> Set.union acc (Set.difference acc x)) xs
+-- Join factored IntMaps tables together, using the max of each value.
+joinTables :: [IntMap.IntMap Integer] -> IntMap.IntMap Integer
+joinTables = foldl (IntMap.unionWith max) IntMap.empty
